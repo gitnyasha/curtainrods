b487eaa3fae023cf2de204b017aef4f3bf3e1dd7
gitnyasha <marshall.chikari@gmail.com>
2020-03-28 16:07:15 +0200

    first commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,19 +15,4 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
-}
-
-function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-}
-
-function onClick(element) {
-    document.getElementById("img01").src = element.src;
-    document.getElementById("modal01").style.display = "block";
-    var captionText = document.getElementById("caption");
-    captionText.innerHTML = element.alt;
-}
+import "bootstrap"
